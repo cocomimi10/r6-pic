b1c63ec8b549f4fce3fccf6921abaa0be689df57
cocomimi10 <135816237+cocomimi10@users.noreply.github.com>
2023-06-07 10:53:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+document.getElementById("randomizeButton").addEventListener("click", function() {
+  var items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]; // Add your own items here
+  var randomIndex = Math.floor(Math.random() * items.length);
+  document.getElementById("result").textContent = items[randomIndex];
+});
 
